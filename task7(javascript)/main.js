6127d13a6ebec80145d3596bf82ed49f8ead4b2b
jaronschulz <jaronschulz@gmail.com>
2017-11-23 14:17:00 +0100

    solved part 3

--- task7(javascript)/main.js
+++ task7(javascript)/main.js
@@ -1,5 +1,26 @@
+// Part 1
 console.log('Starting javascript...');
 
+
+// Part 2
 var name = "Jaron";
 
 console.log(name)
+
+// Part 3
+var myAge = 32;
+
+console.log(myAge)
+
+var averageAge = 30;
+
+var ageDiff = myAge - averageAge;
+
+if (averageAge > myAge) {
+	console.log("I'm younger than the average.");
+} else if (averageAge < myAge) {
+	console.log("I'm older than the average.");
+} else {
+	console.log('I am equal to the average.');
+}
+
